2d69e1d54de1e539a94486a0ee3ee17ee1e54a76
rajni singh <singhrajni2210@gmail.com>
2026-08-10 15:02:54 +0530

    Delete src/ml_foundations directory

--- src/ml_foundations/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Small reference implementations used by the ML Foundations chapters."""
-
-from .linear_regression import LinearRegressionGD
-from .logistic_regression import LogisticRegressionGD
-
-__all__ = ["LinearRegressionGD", "LogisticRegressionGD"]
